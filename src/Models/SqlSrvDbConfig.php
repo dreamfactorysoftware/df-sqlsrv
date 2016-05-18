07f5ac905389be10559dcabd52860bf0c099b2a4
Lee Hicks <leehicks@dreamfactory.com>
2016-05-17 22:51:03 -0400

    updating format of config saved to database

--- src/Models/SqlSrvDbConfig.php
+++ src/Models/SqlSrvDbConfig.php
@@ -11,26 +11,45 @@ class SqlSrvDbConfig extends SqlDbConfig
 {
     public static function getDriverName()
     {
-        if (substr(PHP_OS, 0, 3) == 'WIN') {
-            return 'sqlsrv';
-        } else {
-            return 'dblib';
-        }
-    }
-
-    public static function getDefaultDsn()
-    {
-        if (substr(PHP_OS, 0, 3) == 'WIN') {
-            // http://php.net/manual/en/ref.pdo-sqlsrv.connection.php
-            return 'sqlsrv:Server=localhost,1433;Database=db';
-        } else {
-            // http://php.net/manual/en/ref.pdo-dblib.connection.php
-            return 'dblib:host=localhost:1433;dbname=database;charset=UTF-8';
-        }
+        return 'sqlsrv';
     }
 
     public static function getDefaultPort()
     {
         return 1433;
+    }
+
+    public static function getDefaultConnectionInfo()
+    {
+        $defaults = parent::getDefaultConnectionInfo();
+        if (substr(PHP_OS, 0, 3) == 'WIN') {
+            $defaults[] = [
+                'name'        => 'charset',
+                'label'       => 'Character Set',
+                'type'        => 'string',
+                'description' => 'The character set to use for this connection, i.e. ' . static::getDefaultCharset()
+            ];
+        } else {
+            $defaults[] = [
+                'name'        => 'readonly',
+                'label'       => 'Read Only',
+                'type'        => 'boolean',
+                'description' => 'Defines ApplicationIntent as ReadOnly.'
+            ];
+            $defaults[] = [
+                'name'        => 'pooling',
+                'label'       => 'Enable Connection Pooling',
+                'type'        => 'boolean',
+                'description' => 'Specifies whether the connection is assigned from a connection pool.'
+            ];
+        }
+        $defaults[] = [
+            'name'        => 'appname',
+            'label'       => 'Application Name',
+            'type'        => 'string',
+            'description' => 'The application name used in tracing.'
+        ];
+
+        return $defaults;
     }
 }
