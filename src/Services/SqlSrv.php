3bb95a5ad2edacc011c73c3dd66493d405da19ef
Lee Hicks <leehicks@dreamfactory.com>
2017-12-07 15:32:39 -0500

    Separating resources from resource handlers.

--- src/Services/SqlSrv.php
+++ src/Services/SqlSrv.php
@@ -2,6 +2,8 @@
 
 namespace DreamFactory\Core\SqlSrv\Services;
 
+use DreamFactory\Core\SqlDb\Resources\StoredFunction;
+use DreamFactory\Core\SqlDb\Resources\StoredProcedure;
 use DreamFactory\Core\SqlDb\Services\SqlDb;
 
 /**
@@ -33,6 +35,24 @@ class SqlSrv extends SqlDb
             }
         }
         parent::adaptConfig($config);
+    }
+
+    public function getResourceHandlers()
+    {
+        $handlers = parent::getResourceHandlers();
+
+        $handlers[StoredProcedure::RESOURCE_NAME] = [
+            'name'       => StoredProcedure::RESOURCE_NAME,
+            'class_name' => StoredProcedure::class,
+            'label'      => 'Stored Procedure',
+        ];
+        $handlers[StoredFunction::RESOURCE_NAME] = [
+            'name'       => StoredFunction::RESOURCE_NAME,
+            'class_name' => StoredFunction::class,
+            'label'      => 'Stored Function',
+        ];
+
+        return $handlers;
     }
 
     protected function initStatements($statements = [])
